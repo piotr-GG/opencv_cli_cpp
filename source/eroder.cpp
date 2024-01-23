22a668653f746fd2c21fd7afbf0efe84ee8e1c7e
Piotr Gałecki <herzlicheinladen@gmail.com>
2024-01-23 17:17:08 +0100

    Implementations to eroder

--- source/eroder.cpp
+++ source/eroder.cpp
@@ -7,3 +7,40 @@ void Eroder::process_image(std::vector<std::string> args){
 bool Eroder::process_args(std::vector<std::string> args){
     return false;
 }
+
+cv::Mat Eroder::erode_image(cv::Mat* src_img)
+{
+    return *src_img;
+}
+
+void Eroder::setElement(short val)
+{
+    this->element = val;
+}
+
+short Eroder::getElement()
+{
+    return this->element;
+}
+
+void Eroder::setKernelSize(short val)
+{
+    (*this).kernel_size = val;
+}
+
+short Eroder::getKernelSize()
+{
+    return (*this).kernel_size;
+}
+
+std::array<short, 2> Eroder::getElementLimits()
+{
+    return this->element_limits;
+}
+
+std::array<short, 2> Eroder::getKernelLimits()
+{
+    return this->kernel_limits;
+}
+
+
--- source/include/eroder.h
+++ source/include/eroder.h
@@ -6,4 +6,22 @@ class Eroder: public Transform
     public:
         void process_image(std::vector<std::string> args);
         bool process_args(std::vector<std::string> args);
+        cv::Mat erode_image(cv::Mat* src_img);
+
+        void setElement(short val);
+        short getElement();
+
+        void setKernelSize(short val);
+        short getKernelSize();
+
+        std::array<short, 2> getElementLimits();
+        std::array<short, 2> getKernelLimits();
+
+        bool argumentInRange(short arg_val, const std::array<short, 2>& limits);
+    
+    private:
+        short element = 1;
+        short kernel_size = 3;
+        const std::array<short, 2> element_limits = {0, 2};
+        const std::array<short, 2> kernel_limits = {1, 21};
 };
